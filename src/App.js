3adfaa76d2d0b4002cda2fd5c35560b448048c41
Akil <cyclosis47@gmail.com>
2020-08-18 01:23:26 +0600

    added router completely, fetched from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Header from "./components/layout/Header";
 import AddTodo from "./components/Todo/AddTodo";
 import About from "./components/pages/About";
 import uuid from "uuid";
+import axios from "axios";
 
 class App extends Component {
   state = {
     todos: [
-      { id: uuid.v4(), title: "Clean the room", completed: false },
-      { id: uuid.v4(), title: "Take out the trash", completed: false },
-      { id: uuid.v4(), title: "Buy Groceries", completed: false },
     ],
   };
+
+  async componentDidMount() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    this.setState({
+      todos: response.data
+    })
+  }
 
   markComplete = (id) => {
     this.setState({
@@ -34,7 +39,7 @@ class App extends Component {
   };
 
   addTodo = (todoTitle) => {
-    console.log(todoTitle);
+    
     this.setState({
       todos: [
         ...this.state.todos,
@@ -52,16 +57,20 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Route exact path="/" render={props => (
-            <React.Fragment>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <React.Fragment>
                 <AddTodo addTodo={this.addTodo} />
                 <TodoList
                   todoList={this.state.todos}
                   markComplete={this.markComplete}
                   deleteTodo={this.deleteTodo}
                 />
-            </React.Fragment>
-          )} />
+              </React.Fragment>
+            )}
+          />
 
           <Route path="/about" component={About} />
         </div>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <header style={headerStyle}>
-            <h1>Todo List</h1>
-        </header>
-    )
+  return (
+    <header style={headerStyle}>
+      <h1>Todo List</h1>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+    </header>
+  );
 }
 
 const headerStyle = {
-    textAlign: "center"
-}
-
+  textAlign: "center",
+};
